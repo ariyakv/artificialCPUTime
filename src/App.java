cf9687d2208ee5ef597e3a50f44bd584840b291d
Ariya Kasi Viswanathan <ariyakv@Ariyas-MacBook-Air.local>
2026-05-24 16:08:58 -0400

    App changes 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your username");
+        String username = scanner.nextLine();
+        System.out.println("What is your password");
+        String password = scanner.nextLine();
+        credentialsSetup user = new credentialsSetup(username, password);
+        System.out.println("Here is your username and password");
+        System.out.println(user.getUsername() + " and " + user.getPassword());
+
     }
 }
